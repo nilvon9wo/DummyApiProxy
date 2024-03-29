0a4e6ee0fa688655ffc9480b704f957c32174614
Brian Kessler <kessler.bm@gmail.com>
2024-03-29 19:07:48 +0100

    code cleanup

--- Logging.Serilog/Logging/RequestLoggingStartupFilter.cs
+++ Logging.Serilog/Logging/RequestLoggingStartupFilter.cs
@@ -3,18 +3,23 @@ using Microsoft.AspNetCore.Hosting;
 
 using Serilog;
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace FluxSzerviz.Logging.Serilog.Logging;
 
 /// <summary>
 /// Registers a middleware that runs early in the pipeline and logs incoming requests to serilog.
 /// </summary>
 /// <seealso cref="IStartupFilter" />
+[SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes", Justification = "False negative")]
 internal sealed class RequestLoggingStartupFilter : IStartupFilter
 {
-	public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next) => app =>
-																						   {
-																							   _ = app.UseMiddleware<DetectAzureSlotSwapMiddleware>();
-																							   _ = app.UseSerilogRequestLogging();
-																							   next(app);
-																						   };
+	public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next) 
+		=> app 
+			=>
+			{
+				_ = app.UseMiddleware<DetectAzureSlotSwapMiddleware>();
+				_ = app.UseSerilogRequestLogging();
+				next(app);
+			};
 }
